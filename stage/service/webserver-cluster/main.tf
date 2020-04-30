11079d7aac10962daa9a25cafe25e3f78b2a4926
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-04-30 19:44:46 -0400

    :recycle: move user-data to bash script

--- stage/service/webserver-cluster/main.tf
+++ stage/service/webserver-cluster/main.tf
@@ -17,11 +17,7 @@ resource "aws_launch_configuration" "example" {
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
+  user_data = file("user-data.sh")
 
   lifecycle {
     create_before_destroy = true
