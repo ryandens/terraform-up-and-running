8113778e631a899246a2078d48767abae50aba63
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-04-30 17:01:59 -0400

    :sparkles: define rds resource

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix = "terraform-up-and-running"
+  engine = "mysql"
+  allocated_storage = 10
+  instance_class = "db.t2.micro"
+  name = "admin"
+  password = var.db_password
+}
