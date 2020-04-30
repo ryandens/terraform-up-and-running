8a8d9e7f9aa71e0d8fcba78bd7db5ccd8ad079b5
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-04-30 17:18:39 -0400

    :bug: fix bug where username was specified via wrong field, add terraform backend

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -2,11 +2,22 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  backend "s3" {
+    region         = "us-east-2"
+    bucket         = "terrafrom-state-terraform-up-and-running"
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 resource "aws_db_instance" "example" {
   identifier_prefix = "terraform-up-and-running"
   engine            = "mysql"
   allocated_storage = 10
   instance_class    = "db.t2.micro"
-  name              = "admin"
+  name              = "example_database"
+  username          = "admin"
   password          = var.db_password
 }
