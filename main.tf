338f420be939239eb5276a0f5b4a969cb8aa9267
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-04-30 12:59:01 -0400

    :recycle: move output to outputs.tf

--- main.tf
+++ main.tf
@@ -138,8 +138,3 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
-
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
