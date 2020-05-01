f88580cd824a32d59b606de3f55c601a05dc8234
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-04-30 22:56:46 -0400

    :recycle: pull more stuff into module variables

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
   user_data = data.template_file.user_data.rendered
@@ -27,8 +27,8 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size = var.min_size
+  max_size = var.max_size
 
   tag {
     key                 = "Name"
@@ -38,7 +38,7 @@ resource "aws_autoscaling_group" "example" {
 }
 
 resource "aws_lb" "example" {
-  name               = var.alb_name
+  name               = var.cluster_name
   load_balancer_type = "application"
   subnets            = data.aws_subnet_ids.default.ids
   security_groups    = [aws_security_group.alb.id]
@@ -61,7 +61,7 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_target_group" "asg" {
-  name     = var.alb_name
+  name     = var.cluster_name
   port     = var.server_port
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
@@ -113,15 +113,15 @@ data "terraform_remote_state" "db" {
   backend = "s3"
 
   config = {
-    bucket = "terrafrom-state-terraform-up-and-running"
-    key    = "stage/data-stores/mysql/terraform.tfstate"
+    bucket = var.db_remote_state_bucket
+    key    = var.db_remote_state_key
     region = "us-east-2"
   }
 }
 
 
 resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
+  name = "${var.cluster_name}-alb"
 
   # allow inbound HTTP requests
   ingress {
@@ -142,7 +142,7 @@ resource "aws_security_group" "alb" {
 
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "${var.cluster_name}-instance"
 
   ingress {
     from_port   = var.server_port
--- stage/service/webserver-cluster/main.tf
+++ stage/service/webserver-cluster/main.tf
@@ -3,5 +3,11 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source                 = "../../../modules/services/webserver-cluster"
+  cluster_name           = "webserver-stage"
+  db_remote_state_bucket = "terrafrom-state-terraform-up-and-running"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+  instance_type          = "t2.micro"
+  max_size               = 10
+  min_size               = 2
 }
