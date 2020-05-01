6ec7cc6e9f87e188ce481794cd35a2a0382d6b01
Ryan Dens <ryan.dens@contrastsecurity.com>
2020-04-30 23:14:47 -0400

    :recycle: refactor based on module changes with locals

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -45,6 +45,20 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+resource "aws_security_group" "instance" {
+  name = "${var.cluster_name}-instance"
+}
+
+resource "aws_security_group_rule" "allow_server_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
 resource "aws_lb" "example" {
   name               = var.cluster_name
   load_balancer_type = "application"
@@ -54,7 +68,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   default_action {
@@ -101,13 +115,30 @@ resource "aws_lb_listener_rule" "asg" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_security_group" "alb" {
+  name = "${var.cluster_name}-alb"
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
 
 data "terraform_remote_state" "db" {
   backend = "s3"
@@ -119,35 +150,18 @@ data "terraform_remote_state" "db" {
   }
 }
 
-
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-
-  # allow inbound HTTP requests
-  ingress {
-    from_port   = var.alb_ingress_port
-    to_port     = var.alb_ingress_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # allow all outbound reqeusts
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
 
-resource "aws_security_group" "instance" {
-  name = "${var.cluster_name}-instance"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
